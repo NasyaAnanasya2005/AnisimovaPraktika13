d4ec4fa0a61cdb176232209a5fa456781a04eebe
Nasya <nastena.anisimova.05@mail.ru>
2026-03-03 20:10:56 +0500

    Сортировала людей по возрасту по убыванию

--- base.py
+++ base.py
@@ -2,7 +2,7 @@ import sqlite3
 #Создаем подключение к базе данных (файл database.db будет создан)
 connection  = sqlite3.connect ( 'database .db' )
 cursor =connection.cursor()
-#Получаю средний возраст пользователей для каждого возраста
+"""#Получаю средний возраст пользователей для каждого возраста
 cursor.execute('SELECT age, AVG (age) FROM Users GROUP BY age')
 users = cursor.fetchall()
 for user in users:
@@ -13,6 +13,12 @@ for user in users:
 cursor.execute('SELECT age, AVG (age) FROM Users GROUP BY age HAVING AVG(age) > ?',(30,))
 us = cursor.fetchall()
 for userq in us:
-    print(userq)
+    print(userq)"""
+
+#Выбираю и сортирую пользователей по возрасту по убыванию
+cursor.execute('SELECT username, age FROM Users ORDER BY age DESC')
+users = cursor.fetchall()
+for user in users:
+    print(user)
 
 connection.close()
