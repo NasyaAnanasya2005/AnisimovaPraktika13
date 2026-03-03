9e2f293661c2b4e51a2a3f80d4d1d2a5435029b3
Nasya <nastena.anisimova.05@mail.ru>
2026-03-03 19:19:48 +0500

    Изменила возраст  Олега(изменила запись)

--- base.py
+++ base.py
@@ -2,7 +2,7 @@ import sqlite3
 #Создаем подключение к базе данных (файл database.db будет создан)
 connection  = sqlite3.connect ( 'database .db' )
 cursor =connection.cursor()
-data = [('Nasya', 'Nasya05@mail.ru',20), ('Oleg', 'Oleg05@mail.ru',23), ('Bali', 'Bali05@mail.ru',24), ('Vasya', 'Vasya05@mail.ru',24)]
-cursor.execute('INSERT INTO Users (username, email, age) VALUES (?,?,?)', ('Vasya', 'Vasya05@mail.ru',24))
+
+cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'Oleg'))
 connection.commit()
 connection.close()
