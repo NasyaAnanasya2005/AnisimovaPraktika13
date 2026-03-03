4ea73da53afb6f8edbac609595b19978956d85e2
Nasya <nastena.anisimova.05@mail.ru>
2026-03-03 19:17:27 +0500

    Добавила 4 записи

--- base.py
+++ base.py
@@ -2,8 +2,7 @@ import sqlite3
 #Создаем подключение к базе данных (файл database.db будет создан)
 connection  = sqlite3.connect ( 'database .db' )
 cursor =connection.cursor()
-# Создаем таблицу Users
-cursor.execute('CREATE INDEX idx_email ON Users (email) ')
-# Сохраняем изменения и закрываем соединение
+data = [('Nasya', 'Nasya05@mail.ru',20), ('Oleg', 'Oleg05@mail.ru',23), ('Bali', 'Bali05@mail.ru',24), ('Vasya', 'Vasya05@mail.ru',24)]
+cursor.execute('INSERT INTO Users (username, email, age) VALUES (?,?,?)', ('Vasya', 'Vasya05@mail.ru',24))
 connection.commit()
 connection.close()
