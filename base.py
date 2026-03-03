c1282b892d69b2ecfd96db5fb317ce76ca393f40
Nasya <nastena.anisimova.05@mail.ru>
2026-03-03 19:06:51 +0500

    подключилась к базе, добавила поля

--- base.py
+++ base.py
@@ -1,4 +1,12 @@
 import sqlite3
 #Создаем подключение к базе данных (файл database.db будет создан)
 connection  = sqlite3.connect ( 'database .db' )
-connection.close( )
+cursor =connection.cursor()
+# Создаем таблицу Users
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL, email TEXT NOT NULL, age INTEGER) ''')
+# Сохраняем изменения и закрываем соединение
+connection.commit()
+connection.close()
