a12a9fad8a68f9e5dad3985d48ede126e34681fb
Nasya <nastena.anisimova.05@mail.ru>
2026-03-03 20:14:03 +0500

    Комбинировала операторы для выполнения более сложных запросов

--- base.py
+++ base.py
@@ -13,10 +13,15 @@ for user in users:
 cursor.execute('SELECT age, AVG (age) FROM Users GROUP BY age HAVING AVG(age) > ?',(30,))
 us = cursor.fetchall()
 for userq in us:
-    print(userq)"""
+    print(userq)
 
 #Выбираю и сортирую пользователей по возрасту по убыванию
 cursor.execute('SELECT username, age FROM Users ORDER BY age DESC')
+users = cursor.fetchall()
+for user in users:
+    print(user)"""
+#Комбинировала операторы для выполнения более сложных запросов
+cursor.execute('''SELECT username, age, AVG(age) FROM Users GROUP BY age HAVING AVG(age) > ? ORDER BY age DESC ''', (20,))
 users = cursor.fetchall()
 for user in users:
     print(user)
