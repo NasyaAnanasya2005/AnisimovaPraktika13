1e112d965ee2b81888a50f17427c9131058c71b2
Nasya <nastena.anisimova.05@mail.ru>
2026-03-03 19:20:59 +0500

    Удалила запись которую изменила, а именно Олега

--- base.py
+++ base.py
@@ -3,6 +3,6 @@ import sqlite3
 connection  = sqlite3.connect ( 'database .db' )
 cursor =connection.cursor()
 
-cursor.execute('UPDATE Users SET age = ? WHERE username = ?', (29, 'Oleg'))
+cursor.execute('DELETE FROM Users  WHERE username = ?', ('Oleg',))
 connection.commit()
 connection.close()
